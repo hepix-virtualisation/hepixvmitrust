8460e04c1f58801cd01ffdb56779669b563aa944
osynge <owen.synge@desy.de>
2011-03-07 17:31:08 +0000

    tagging a release
    git-svn-id: https://svnsrv.desy.de/public/grid-virt/org.desy.grid-virt.simple-python-signing/trunk@53 948ea017-ca80-42ba-9c89-43e3e1f18474

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='desy-grid-vmis',
-    version='0.0.2',
+    version='0.0.3',
     description="Simple straw man signer",
     author="O M Synge",
     author_email="owen.Synge@desy.de",
