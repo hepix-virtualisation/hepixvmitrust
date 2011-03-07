da60443838b44b6ab8eabde38b4c1dff53c79773
osynge <owen.synge@desy.de>
2011-03-07 18:03:48 +0000

    make the script vmilisttool.py executable
    git-svn-id: https://svnsrv.desy.de/public/grid-virt/org.desy.grid-virt.simple-python-signing/trunk@55 948ea017-ca80-42ba-9c89-43e3e1f18474

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 setup(name='desy-grid-vmis',
-    version='0.0.3',
+    version='0.0.4',
     description="Simple straw man signer",
     author="O M Synge",
     author_email="owen.Synge@desy.de",
     url="www-it.desy.de",
-    data_files=[('/usr/bin/', ['vmilisttool.py']),
-        ('/usr/share/doc/vmilisttool',['README','ChangeLog','imagelist.json'])]
+    scripts=['vmilisttool.py'],
+    data_files=[('/usr/share/doc/vmilisttool',['README','ChangeLog','imagelist.json'])]
     )
